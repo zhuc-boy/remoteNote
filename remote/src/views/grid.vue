ecaa82b7aa12d6754306fdbdf7fba222a7b1ff19
cheng.zhu <cheng.zhu@gareatech.com>
2020-07-15 11:25:56 +0800

    苹果11的底部安全距离，横竖屏多媒体，安卓机点击元素有蓝色亮块问题

--- remote/src/views/grid.vue
+++ remote/src/views/grid.vue
@@ -6,11 +6,11 @@
                 <div data-item="clear">C</div>
                 <div data-item="colc">+/-</div>
                 <div data-item="colc">%</div>
-                <div data-item="">/</div>
+                <div data-item="">&divide;</div>
                 <div data-item="bt">7</div>
                 <div data-item="bt">8</div>
                 <div data-item="bt">9</div>
-                <div data-item="">*</div>
+                <div data-item="">&times;</div>
                 <div data-item="bt">4</div>
                 <div data-item="bt">5</div>
                 <div data-item="bt">6</div>
@@ -76,67 +76,73 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-.middle {
-    display: flex;
-    // align-items: center;
-    // vertical-align: middle;
-    flex-direction: column;
-    .Num {
-        line-height: 50px;
-        font-size: calc((100vw - 20px) / 6);
-        text-align: right;
-        padding: 15px;
-    }
-    .calcBox {
-        position: fixed;
-        bottom: 0;
-    }
+@media all and (orientation: landscape) {
+    /* 横屏 */
 }
-.gridbox {
-    -webkit-touch-callout: none; /* iOS Safari */
-    -webkit-user-select: none; /* Chrome/Safari/Opera */
-    -khtml-user-select: none; /* Konqueror */
-    -moz-user-select: none; /* Firefox */
-    -ms-user-select: none; /* Internet Explorer/Edge */
-    user-select: none; /* Non-prefixed version, currently not supported by any browser */
-    // margin: 0px auto;
-    display: grid;
-    grid-template-columns: repeat(4, calc((100vw - 30px) / 4));
-    grid-template-rows: repeat(5, calc((100vw - 30px) / 4));
-    grid-gap: 10px;
-    grid-template-areas:
-        "a b c j"
-        "d e f k"
-        "m n o p"
-        "q r s t"
-        "g g i l";
-    .zero {
-        grid-area: g;
-        border-radius: calc((100vw - 30px) / 8);
-        // transform: scaleX(0.9);
+@media all and (orientation: portrait) {
+    /* 竖屏 */
+    .middle {
+        display: flex;
+        // align-items: center;
+        // vertical-align: middle;
+        flex-direction: column;
+        .Num {
+            line-height: 50px;
+            font-size: calc((100vw - 20px) / 6);
+            text-align: right;
+            padding: 15px;
+        }
+        .calcBox {
+            position: absolute;
+            bottom: calc(env(safe-area-inset-bottom));
+        }
     }
+    .gridbox {
+        -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Chrome/Safari/Opera */
+        -khtml-user-select: none; /* Konqueror */
+        -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none; /* Non-prefixed version, currently not supported by any browser */
+        // margin: 0px auto;
+        display: grid;
+        grid-template-columns: repeat(4, calc((100vw - 30px) / 4));
+        grid-template-rows: repeat(5, calc((100vw - 30px) / 4));
+        grid-gap: 10px;
+        grid-template-areas:
+            "a b c j"
+            "d e f k"
+            "m n o p"
+            "q r s t"
+            "g g i l";
+        .zero {
+            grid-area: g;
+            border-radius: calc((100vw - 30px) / 8);
+            transform: scale(0.9, 0.8);
+        }
 
-    & > div {
-        // border: 1px solid black;
-        transform: scale(0.8);
-        border-radius: 50%;
-        line-height: calc((100vw - 30px) / 4);
-        font-size: calc((100vw - 30px) / 12);
-        text-align: center;
-        vertical-align: middle;
-        background-color: #fff;
-        box-shadow: 5px 5px 5px #222;
-        place-items: center;
-        cursor: pointer;
-    }
-    & > div:hover {
-        box-shadow: -2px 0px 1px #222;
-    }
-    & > div.actived {
-        box-shadow: -2px 0px 1px #222;
-    }
-    & > div.deactived {
-        box-shadow: 5px 5px 5px #222;
+        & > div {
+            // border: 1px solid black;
+            transform: scale(0.8);
+            border-radius: 50%;
+            line-height: calc((100vw - 30px) / 4);
+            font-size: calc((100vw - 30px) / 12);
+            text-align: center;
+            vertical-align: middle;
+            background-color: #fff;
+            box-shadow: 5px 5px 5px #222;
+            place-items: center;
+            cursor: pointer;
+        }
+        & > div:hover {
+            box-shadow: -2px 0px 1px #222;
+        }
+        & > div.actived {
+            box-shadow: -2px 0px 1px #222;
+        }
+        & > div.deactived {
+            box-shadow: 5px 5px 5px #222;
+        }
     }
 }
 </style>
